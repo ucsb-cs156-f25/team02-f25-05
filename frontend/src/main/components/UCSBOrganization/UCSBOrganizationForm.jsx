2a3011e3f1e4d13d9ca4bc848fa131565b026fb8
Mon Fernandez <monraphael@ucsb.edu>
2025-10-31 01:06:56 -0700

    MF - seeing if test works.

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.jsx
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationForm.jsx
@@ -57,7 +57,6 @@ function UCSBOrganizationForm({
           Organization Translation
         </Form.Label>
         <Form.Control
-          data-testid={testIdPrefix + "-orgTranslation"}
           id="orgTranslation"
           type="text"
           isInvalid={Boolean(errors.orgTranslation)}
@@ -72,19 +71,13 @@ function UCSBOrganizationForm({
 
       <Form.Group className="mb-3">
         <Form.Label htmlFor="inactive">Inactive Status</Form.Label>
-        <Form.Select
-          data-testid={testIdPrefix + "-inactive"}
-          id="inactive"
-          {...register("inactive", {})}
-        >
+        <Form.Select id="inactive" {...register("inactive", {})}>
           <option value="true">True</option>
           <option value="false">False</option>
         </Form.Select>
       </Form.Group>
 
-      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
-        {buttonLabel}
-      </Button>
+      <Button type="submit">{buttonLabel}</Button>
       <Button
         variant="Secondary"
         onClick={() => navigate(-1)}
