9e2b214749777fb6081c58b3227f16abfbba0f8c
Brian Sen <bsen@ucsb.edu>
2025-11-04 18:07:27 -0800

    form fixes

--- frontend/src/main/components/HelpRequest/HelpRequestForm.jsx
+++ frontend/src/main/components/HelpRequest/HelpRequestForm.jsx
@@ -1,196 +1,33 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router";
+import React from "react";
+import HelpRequestForm from "main/components/HelpRequest/HelpRequestForm";
+import { helpRequestFixtures } from "fixtures/helpRequestFixtures";
 
-function HelpRequestForm({
-  initialContents,
-  submitAction,
-  buttonLabel = "Create",
-}) {
-  // Stryker disable all
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({ defaultValues: initialContents || {} });
-  // Stryker restore all
+export default {
+  title: "components/HelpRequest/HelpRequestForm",
+  component: HelpRequestForm,
+};
 
-  const navigate = useNavigate();
+const Template = (args) => {
+  return <HelpRequestForm {...args} />;
+};
 
-  // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
-  // Note that even this complex regex may still need some tweaks
+export const Create = Template.bind({});
 
-  // Stryker disable Regex
-  const isodate_regex =
-    /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
-  const id_regex = /^\d+$/;
-  const email_regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  // Stryker restore Regex
+Create.args = {
+  buttonLabel: "Create",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
 
-  return (
-    <Form onSubmit={handleSubmit(submitAction)}>
-      <Row>
-        {initialContents && (
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label htmlFor="id">Id</Form.Label>
-              <Form.Control
-                data-testid="HelpRequestForm-id"
-                id="id"
-                type="text"
-                {...register("id")}
-                value={initialContents.id}
-                disabled
-              />
-            </Form.Group>
-          </Col>
-        )}
+export const Update = Template.bind({});
 
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="requesterEmail">Requester Email</Form.Label>
-            <Form.Control
-              data-testid="HelpRequestForm-requesterEmail"
-              id="requesterEmail"
-              type="text"
-              placeholder="e.g. davidgaucho@ucsb.edu"
-              isInvalid={Boolean(errors.requesterEmail)}
-              {...register("requesterEmail", {
-                required: "Requester Email is required.",
-                pattern: {
-                  value: email_regex,
-                  message: "Requester Email must be a valid email address.",
-                },
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.requesterEmail?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="teamId">Team Id</Form.Label>
-            <Form.Control
-              data-testid="HelpRequestForm-teamId"
-              id="teamId"
-              type="text"
-              placeholder="e.g. 04"
-              isInvalid={Boolean(errors.teamId)}
-              {...register("teamId", {
-                required: "Team Id is required.",
-                pattern: {
-                  value: id_regex,
-                  message: "Team Id must be a number.",
-                },
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.teamId?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="tableOrBreakoutRoom">
-              Table Or Breakout Room
-            </Form.Label>
-            <Form.Control
-              data-testid="HelpRequestForm-tableOrBreakoutRoom"
-              id="tableOrBreakoutRoom"
-              type="text"
-              placeholder="e.g. 04"
-              isInvalid={Boolean(errors.tableOrBreakoutRoom)}
-              {...register("tableOrBreakoutRoom", {
-                required: "Table Or Breakout Room is required.",
-                pattern: {
-                  value: id_regex,
-                  message: "Table Or Breakout Room must be a number.",
-                },
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.tableOrBreakoutRoom?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="requestTime">
-              Request Time (ISO Format)
-            </Form.Label>
-            <Form.Control
-              data-testid="HelpRequestForm-requestTime"
-              id="requestTime"
-              type="datetime-local"
-              isInvalid={Boolean(errors.requestTime)}
-              {...register("requestTime", {
-                required: true,
-                pattern: isodate_regex,
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.requestTime && "Request Time is required."}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="explanation">Explanation</Form.Label>
-            <Form.Control
-              data-testid="HelpRequestForm-explanation"
-              id="explanation"
-              type="text"
-              placeholder="e.g. Gradescope tests failing."
-              isInvalid={Boolean(errors.explanation)}
-              {...register("explanation", {
-                required: "Explanation is required.",
-                maxLength: {
-                  value: 250,
-                  message: "Explanation must be at most 250 characters.",
-                },
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.explanation?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Col>
-
-        <Col>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="solved">Solved</Form.Label>
-            <Form.Check
-              data-testid="HelpRequestForm-solved"
-              id="solved"
-              type="switch"
-              isInvalid={Boolean(errors.solved)}
-              {...register("solved")}
-            />
-          </Form.Group>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col>
-          <Button type="submit" data-testid="HelpRequestForm-submit">
-            {buttonLabel}
-          </Button>
-          <Button
-            variant="Secondary"
-            onClick={() => navigate(-1)}
-            data-testid="HelpRequestForm-cancel"
-          >
-            Cancel
-          </Button>
-        </Col>
-      </Row>
-    </Form>
-  );
-}
-
-export default HelpRequestForm;
+Update.args = {
+  initialContents: helpRequestFixtures.oneHelpRequest,
+  buttonLabel: "Update",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
