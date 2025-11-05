5f434af76597e7a347686504f73bdfec089c4aa4
Brian Sen <bsen@ucsb.edu>
2025-11-04 21:22:23 -0800

    Create page

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -2,8 +2,8 @@ const helpRequestFixtures = {
   oneRequest: {
     id: 1,
     requesterEmail: "student@ucsb.edu",
-    teamId: "team01",
-    tableOrBreakoutRoom: "Table 5",
+    teamId: "01",                  // was "team01"
+    tableOrBreakoutRoom: "5",      // was "Table 5"
     explanation: "Having trouble connecting to the database.",
     solved: false,
     requestTime: "2022-01-02T12:00:00",
@@ -12,8 +12,8 @@ const helpRequestFixtures = {
     {
       id: 1,
       requesterEmail: "student1@ucsb.edu",
-      teamId: "team01",
-      tableOrBreakoutRoom: "Table 5",
+      teamId: "01",                // was "team01"
+      tableOrBreakoutRoom: "5",    // was "Table 5"
       explanation: "Can't deploy to Heroku.",
       solved: false,
       requestTime: "2022-01-02T12:00:00",
@@ -21,8 +21,8 @@ const helpRequestFixtures = {
     {
       id: 2,
       requesterEmail: "student2@ucsb.edu",
-      teamId: "team02",
-      tableOrBreakoutRoom: "Breakout Room 1",
+      teamId: "02",                // was "team02"
+      tableOrBreakoutRoom: "1",    // was "Breakout Room 1"
       explanation: "React app not compiling.",
       solved: true,
       requestTime: "2022-04-03T12:00:00",
@@ -30,8 +30,8 @@ const helpRequestFixtures = {
     {
       id: 3,
       requesterEmail: "student3@ucsb.edu",
-      teamId: "team03",
-      tableOrBreakoutRoom: "Table 7",
+      teamId: "03",                // was "team03"
+      tableOrBreakoutRoom: "7",    // was "Table 7"
       explanation: "Need help with Docker setup.",
       solved: false,
       requestTime: "2022-07-04T12:00:00",
