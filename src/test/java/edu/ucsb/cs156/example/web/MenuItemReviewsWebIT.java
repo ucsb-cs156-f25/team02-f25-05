1b81ffeb849e069e567603375225462aeac05bc8
Melvin Mathew <amelvinmathew@gmail.com>
2025-11-06 20:44:59 -0800

    mm - fixed e2e tests

--- src/test/java/edu/ucsb/cs156/example/web/MenuItemReviewsWebIT.java
+++ src/test/java/edu/ucsb/cs156/example/web/MenuItemReviewsWebIT.java
@@ -1,71 +1,51 @@
-package edu.ucsb.cs156.example.integration;
+package edu.ucsb.cs156.example.web;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.ucsb.cs156.example.WebTestCase;
-import edu.ucsb.cs156.example.entities.MenuItemReviews;
-import edu.ucsb.cs156.example.repositories.MenuItemReviewsRepository;
-import edu.ucsb.cs156.example.repositories.UserRepository;
-import edu.ucsb.cs156.example.services.CurrentUserService;
-import edu.ucsb.cs156.example.services.GrantedAuthoritiesService;
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@AutoConfigureMockMvc
 @ActiveProfiles("integration")
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MenuItemReviewsWebIT extends WebTestCase {
-  @Autowired public CurrentUserService currentUserService;
-
-  @Autowired public GrantedAuthoritiesService grantedAuthoritiesService;
-
-  @Autowired MenuItemReviewsRepository menuItemReviewsRepository;
-
-  @Autowired public MockMvc mockMvc;
-
-  @Autowired public ObjectMapper mapper;
-
-  @MockitoBean UserRepository userRepository;
-
-  @WithMockUser(roles = {"USER"})
   @Test
-  public void admin_user_can_see_existing_entries() throws Exception {
-    // arrange: create a MenuItemReview directly in the repository
-    LocalDateTime dateReviewed = LocalDateTime.parse("2023-01-01T12:00:00");
-    MenuItemReviews review =
-        MenuItemReviews.builder()
-            .itemId(2L)
-            .reviewerEmail("test@gmail.com")
-            .stars(3)
-            .dateReviewed(dateReviewed)
-            .comments("great")
-            .build();
-
-    menuItemReviewsRepository.save(review);
-
-    // act: log in as admin and go to the page
+  public void admin_user_can_create_edit_delete_menuitemreview() throws Exception {
     setupUser(true);
 
+    assertThat(page.getByText("Menu Item Reviews")).isVisible();
     page.getByText("Menu Item Reviews").click();
 
-    // assert: confirm that the entry is visible
+    assertThat(page.getByText("Create MenuItemReviews")).isVisible();
+    page.getByText("Create MenuItemReviews").click();
+    // assertThat(page.getByText("Create")).isVisible();
+    page.getByTestId("MenuItemReviewsForm-itemId").fill("2");
+    page.getByTestId("MenuItemReviewsForm-reviewerEmail").fill("test@gmail.com");
+    page.getByTestId("MenuItemReviewsForm-stars").fill("3");
+    page.getByTestId("MenuItemReviewsForm-dateReviewed").fill("2023-01-01T12:00");
+    page.getByTestId("MenuItemReviewsForm-comments").fill("great");
+    page.getByTestId("MenuItemReviewsForm-submit").click();
+
     assertThat(page.getByTestId("MenuItemReviewsTable-cell-row-0-col-comments")).hasText("great");
+
+    page.getByTestId("MenuItemReviewsTable-cell-row-0-col-Edit-button").click();
+    assertThat(page.getByText("Edit Menu Item Review")).isVisible();
+    page.getByTestId("MenuItemReviewsForm-comments").fill("THE BEST");
+    page.getByTestId("MenuItemReviewsForm-submit").click();
+
+    assertThat(page.getByTestId("MenuItemReviewsTable-cell-row-0-col-comments"))
+        .hasText("THE BEST");
+
+    page.getByTestId("MenuItemReviewsTable-cell-row-0-col-Delete-button").click();
+
+    assertThat(page.getByTestId("MenuItemReviewsTable-cell-row-0-col-itemId")).not().isVisible();
   }
 
   @Test
